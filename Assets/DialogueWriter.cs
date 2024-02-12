b574a667e5529de16b582a903d65f707d868685d
BreadBabe <143431412+BreadBabe@users.noreply.github.com>
2024-02-12 13:38:53 +0100

    Writing Script
    Uploading script for writing out text letter for letter in dialogue box

---
+++ Assets/DialogueWriter.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DialogueWriter : MonoBehaviour
+{
+    //Text as shown on screen
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI dialogueSpeaker;
+
+    //Imported dialogue text
+    private string[] TextArray;
+    private string importedText;
+
+    //Stringbuilder
+    StringBuilder incrementalSB;
+
+    //
+    private int sentenceCount;
+    private int currentLetter;
+    private float writingDelay;
+    private float timeSinceLetter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        incrementalSB = new StringBuilder();
+        dialogueText = GetComponent<TextMeshProUGUI>();
+        importedText = "Hello World i am on TV";
+        importedText.ToCharArray();
+        sentenceCount = importedText.Count();
+        writingDelay = 0.1f;
+        currentLetter = 0;
+        dialogueText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextWriter();
+
+    }
+
+    private void TextWriter()
+    {
+        if (timeSinceLetter + writingDelay < Time.realtimeSinceStartup)
+        {
+            if (currentLetter != sentenceCount)
+            {
+                timeSinceLetter = Time.realtimeSinceStartup;
+                incrementalSB.Append(importedText[currentLetter]);
+                dialogueText.text = incrementalSB.ToString();
+                currentLetter++;
+            }
+            else
+            {
+                incrementalSB.Append("ERROR");
+            }
+        }
+    }
+}
