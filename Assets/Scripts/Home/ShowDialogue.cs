827354409c356afe45926c828eaab2fed7e08bdf
emelemb <143512378+emelemb@users.noreply.github.com>
2024-04-12 15:51:44 +0200

    fixed the structure and orginastion of scenes, dialogs n shit

--- Assets/Scripts/Home/ShowDialogue.cs
+++ Assets/Scripts/Home/ShowDialogue.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class DiaTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
+    public UI.Button yourButton;
     void Start()
     {
-        
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("button works fr lets fucking go");
     }
 
     // Update is called once per frame
