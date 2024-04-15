6edc3d674cc8b1abd9f88ec61d218c1c79451f08
ylvz <ylvoos@msn.com>
2024-04-15 13:16:32 +0200

    Luna
    hej

---
+++ Assets/Luna/Date 1/Scripts/LunaInkManager.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Ink.Runtime;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LunaInkManager : MonoBehaviour
+{
+    public static event Action<Story> OnCreateStory;
+
+    [SerializeField] private float typingSpeed;
+
+    private Text storyText;
+    private string currentText;
+    private bool isTyping;
+
+    void Awake()
+    {
+        RemoveChildren();
+        StartStory();
+    }
+
+    void StartStory()
+    {
+        story = new Story(inkJSONAsset.text);
+        if (OnCreateStory != null) OnCreateStory(story);
+        RefreshView();
+    }
+
+    void RefreshView()
+    {
+        RemoveChildren();
+
+        string text = story.Continue().Trim();
+        currentText = "";
+        isTyping = true;
+        CreateContentView(text); // Call CreateContentView with the obtained text
+    }
+
+
+
+    void OnClickChoiceButton(Choice choice)
+    {
+        story.ChooseChoiceIndex(choice.index);
+        RefreshView();
+    }
+
+    void CreateContentView(string text)
+    {
+        storyText = Instantiate(textPrefab) as Text;
+        storyText.text = ""; // Start with empty text
+        storyText.transform.SetParent(dialoguebox.transform, false);
+        StartCoroutine(TypeText(text)); // Start typing effect
+    }
+
+
+
+    Button CreateChoiceView(string text)
+    {
+        Button choice = Instantiate(buttonPrefab) as Button;
+        choice.transform.SetParent(choicebox.transform, false);
+
+        Text choiceText = choice.GetComponentInChildren<Text>();
+        choiceText.text = text;
+
+        HorizontalLayoutGroup layoutGroup = choice.GetComponent<HorizontalLayoutGroup>();
+        layoutGroup.childForceExpandHeight = false;
+
+        return choice;
+    }
+
+    void RemoveChildren()
+    {
+        foreach (Transform child in dialoguebox.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in choicebox.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    IEnumerator TypeText(string text)
+    {
+        float timePerCharacter = 1f / typingSpeed; // Calculate time per character based on typing speed
+        foreach (char letter in text)
+        {
+            currentText += letter;
+            storyText.text = currentText;
+            yield return new WaitForSeconds(timePerCharacter); // Wait for timePerCharacter seconds per character
+        }
+
+        isTyping = false;
+        DisplayChoices(); // Display choices after typing is complete
+    }
+
+
+
+    void DisplayChoices()
+    {
+        if (!story.canContinue && !isTyping)
+        {
+            if (story.currentChoices.Count > 0)
+            {
+                for (int i = 0; i < story.currentChoices.Count; i++)
+                {
+                    Choice choice = story.currentChoices[i];
+                    Button button = CreateChoiceView(choice.text.Trim());
+                    button.onClick.AddListener(() => OnClickChoiceButton(choice));
+                }
+            }
+            else
+            {
+                Button choice = CreateChoiceView("End of story.\nRestart?");
+                choice.onClick.AddListener(delegate {
+                    StartStory();
+                });
+            }
+        }
+    }
+
+    [SerializeField] private TextAsset inkJSONAsset = null;
+    private Story story;
+
+    [SerializeField] private GameObject dialoguebox = null;
+    [SerializeField] private GameObject choicebox = null;
+
+    [SerializeField] private Text textPrefab = null;
+
+    [SerializeField] private Button buttonPrefab = null;
+}
+
+
