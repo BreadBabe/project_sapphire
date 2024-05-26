846fadb0ad0473be20d5bf84697ada8cdff2a5f1
BreadBabe <143431412+BreadBabe@users.noreply.github.com>
2024-05-26 13:37:15 +0200

    Fixed a small error

--- Assets/Main Menu/Scripts/ButtonBehaviourScript.cs
+++ Assets/Main Menu/Scripts/ButtonBehaviourScript.cs
@@ -18,6 +18,7 @@ public class ButtonBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PlayerPrefs.DeleteAll();
         sceneChanger = new SceneChanger();
         startbutton.GetComponent<Button>().onClick.AddListener(OnClick);
     }
