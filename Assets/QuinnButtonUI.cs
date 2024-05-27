091d6878f9bc5a16e7e533e42a4804f93408682f
vilma04 <143352488+vilma04@users.noreply.github.com>
2024-05-27 19:59:03 +0200

    27/5
    Quinn fixad

--- Assets/QuinnButtonUI.cs
+++ Assets/QuinnButtonUI.cs
@@ -5,42 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class QuinnButtonUI : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] private string newGameLevel = "Level1";
-    //public GameObject BookStore;
 
     private GameObject currentPicture;
     private GameObject currentLeftText;
     private GameObject currentRightText;
     private GameObject currentYesButton;
-    //private GameObject backText;
+    private GameObject currentImage;
 
     [SerializeField] private GameObject backText;
     [SerializeField] private GameObject backWrongText;
-    //[SerializeField] private List<GameObject> backTextList = new List<GameObject>();
     [SerializeField] private List<GameObject> pictureList = new List<GameObject>();
     [SerializeField] private List<GameObject> textLeftList = new List<GameObject>();
     [SerializeField] private List<GameObject> textRightList = new List<GameObject>();
     [SerializeField] private List<GameObject> buttonYesList = new List<GameObject>();
-    //Kan man inte sötta en serializefield för dialog tbx och en bool till det?
-
+    [SerializeField] private List<GameObject> imageList = new List<GameObject>();
 
     public bool isClicked = false;
     int index = 0;
     public bool isPressed = false;
-
 
     public void NewGameButton()
     {
         SceneManager.LoadScene(newGameLevel);
     }
 
-
-    public void BookBackground()
+    private void ResetUI()
     {
-        currentPicture.SetActive(false);
-        backText.SetActive(false);
-        backWrongText.SetActive(false);
         foreach (GameObject textObj in textLeftList)
         {
             textObj.SetActive(false);
@@ -53,17 +44,33 @@ public class QuinnButtonUI : MonoBehaviour
         {
             buttonYesObj.SetActive(false);
         }
-        if (index + 1 == pictureList.Count) return;
-
-
+        foreach (GameObject imageObj in imageList)
+        {
+            imageObj.SetActive(false);
+        }
+        foreach (GameObject pictureObj in pictureList)
+        {
+            pictureObj.SetActive(false);
+        }
+        backText.SetActive(false);
+        backWrongText.SetActive(false);
     }
+
+    public void BookBackground()
+    {
+        ResetUI();
+        if (index + 1 == pictureList.Count) return;
+    }
+
     public void BookBackground2()
     {
         currentPicture.SetActive(true);
         currentLeftText.SetActive(true);
         currentRightText.SetActive(true);
         currentYesButton.SetActive(true);
+        currentImage.SetActive(true);
     }
+
     public void YesButton()
     {
         BookBackground();
@@ -72,17 +79,25 @@ public class QuinnButtonUI : MonoBehaviour
         backWrongText.SetActive(true);
         backText.SetActive(true);
     }
+
     public void Yes2Button()
     {
-        //Byta scen
     }
+
     public void NoButton()
     {
         BookBackground();
         currentPicture = pictureList[0];
         currentPicture.SetActive(true);
         backText.SetActive(true);
+        // Reset current objects to be able to click again
+        currentLeftText = textLeftList[0];
+        currentRightText = textRightList[0];
+        currentYesButton = buttonYesList[0];
+        currentImage = imageList[0];
+        BookBackground2();
     }
+
     public void ChangeBackground()
     {
         BookBackground();
@@ -100,8 +115,8 @@ public class QuinnButtonUI : MonoBehaviour
         currentLeftText = textLeftList[1];
         currentRightText = textRightList[1];
         currentYesButton = buttonYesList[0];
+        currentImage = imageList[1];
         BookBackground2();
-
     }
 
     public void YellowBookBackground()
@@ -111,8 +126,8 @@ public class QuinnButtonUI : MonoBehaviour
         currentLeftText = textLeftList[2];
         currentRightText = textRightList[2];
         currentYesButton = buttonYesList[0];
+        currentImage = imageList[2];
         BookBackground2();
-
     }
 
     public void RedBookBackground()
@@ -122,34 +137,18 @@ public class QuinnButtonUI : MonoBehaviour
         currentLeftText = textLeftList[3];
         currentRightText = textRightList[3];
         currentYesButton = buttonYesList[0];
+        currentImage = imageList[3];
         BookBackground2();
-
     }
 
     public void BlueBookBackground()
     {
-        //BookBackground();
-        currentPicture.SetActive(false);
-        backText.SetActive(false);
-        backWrongText.SetActive(false);
-        foreach (GameObject textObj in textLeftList)
-        {
-            textObj.SetActive(false);
-        }
-        foreach (GameObject textRightObj in textRightList)
-        {
-            textRightObj.SetActive(false);
-        }
-        foreach (GameObject buttonYesObj in buttonYesList)
-        {
-            buttonYesObj.SetActive(false);
-        }
-        if (index + 1 == pictureList.Count) return;
-
+        BookBackground();
         currentPicture = pictureList[1];
         currentLeftText = textLeftList[4];
         currentRightText = textRightList[4];
         currentYesButton = buttonYesList[1];
+        currentImage = imageList[4];
         BookBackground2();
     }
 
@@ -161,6 +160,10 @@ public class QuinnButtonUI : MonoBehaviour
         currentPicture = pictureList[0];
         currentLeftText = textLeftList[0];
         currentRightText = textRightList[0];
+        currentImage = pictureList[0];
         backWrongText.SetActive(false);
     }
 }
+
+
+
